7b84df37549da927f74ddc0a44758075c783b3c0
Lutz Lengemann <lutz@lengemann.net>
2021-01-10 21:51:42 +0100

    Correct sort order reference

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,17 @@ class Post < ApplicationRecord
   has_many :post_tags
   has_many :tags, through: :post_tags
 
+  # NOTE: The sort_index is depended and the data from USER not POST!!!
   SELECTS = Arel.sql(
     <<~SQL.squish
       posts.*,
-      CASE posts.order
-      WHEN 1 THEN 1
+      CASE users.order
+      WHEN 5 THEN 0
+      WHEN 4 THEN 1
+      WHEN 3 THEN 2
+      WHEN 2 THEN 3
+      WHEN 1 THEN 4
+      WHEN 0 THEN 5
       ELSE 1000
       END as sort_index
     SQL
